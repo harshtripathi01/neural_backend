48332c99a1437e034197d978e8155db6dcb056b1
1998kapil <kapil.pawar@escalesolutions.com>
2024-07-19 17:57:26 +0530

    setup postgres database

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
-// server.js
-
-const app = require("./app");
 require('dotenv').config();
+const express = require('express');
+const { Sequelize } = require('sequelize');
 
-let port = process.env.PORT || 3000;
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.listen(port, function (err) {
-  if (err) {
-    console.log("Error in start server", err);
-    return;
-  }
-  console.log(`your app is running on Port : ${port}`);
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect: 'postgres'
+});
+
+sequelize.authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+app.listen(port, () => {
+  console.log(`Your app is running on Port: ${port}`);
 });
